56308c5ec41d7e15437d1efc4219c8f92f163d49
shushanfx <shushanfx@gmail.com>
2014-09-03 22:29:29 +0800

    First to upload the source.

---
+++ src/main/java/com/shushanfx/commons/io/IOTest.java
@@ -0,0 +1,10 @@
+package com.shushanfx.commons.io;
+
+/**
+ * Created by shushanfx-ubantu on 9/3/14.
+ */
+public class IOTest {
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+    }
+}
